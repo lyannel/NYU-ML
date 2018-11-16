35021ef1ab3b1a91f678994acfeb0c7a7e67d282
pholoubek <ph1335@nyu.edu>
2018-11-16 18:04:08 -0500

    "New Commit"

--- CryptoFetchtron/app.py
+++ CryptoFetchtron/app.py
@@ -1,6 +1,7 @@
 import ccxt
 import time
 import random
+import sqlite3
 
 """
     CryptoFetch is a crude app maintained by NYU-ML Club.
@@ -21,9 +22,20 @@ class CryptoFetchtron:
         self.exchange = exchange_class({
             'enableRateLimit': True,
         })
-        self.exchange.load_markets()
+    
+    def connect_to_db(self, path):
+        """
+            This specifies where file with db should be stored
+            :@param: path is string with absolute path to the directory where data
+                     from exchange should be stored
 
-    def get_data_current(self, pair="BTC/USDT", limit=20, delay=60):
+            TODO: we should make default behavior to create dir in the current working directory
+        """
+        conn = sqlite3.connect(path + "crypto.sqlite")
+        self.db = conn.cursor()
+
+    #  this queries orderbook 
+    def get_current_orderbook(self, pair="BTC/USDT", limit=5, delay=10):
         """
             :@param limit: integer - how many order book elements are fetched at once.
             :@param delay: integer - how many seconds to wait before next fetch of order book is made
@@ -33,19 +45,28 @@ class CryptoFetchtron:
         if self.exchange.has['fetchOrders']:
             while True:
                 orderbook = self.exchange.fetch_order_book(pair, limit)
-                bids = orderbook['bids']
-                asks = orderbook['asks']
-                current_time = time.asctime(time.localtime(time.time()))
-                for i in range(len(bids)):
-                    print("Pair: {}. Timestamp:{}.\nOrder price: ${} @ Quantity of {} and Asks price: ${} @ Quantity of {}.\n"
-                            .format(pair, current_time, bids[i][0], bids[i][1], asks[i][0], asks[i][1]))
+                print orderbook
+                # bids = orderbook['bids']
+                # asks = orderbook['asks']
+                # current_time = time.asctime(time.localtime(time.time()))
+                # for i in range(len(bids)):
+                #     print("Pair: {}. Timestamp:{}.\nOrder price: ${} @ Quantity of {} and Asks price: ${} @ Quantity of {}.\n"
+                #             .format(pair, current_time, bids[i][0], bids[i][1], asks[i][0], asks[i][1]))
                 time.sleep(delay)
 
-    # def get_data_history(self, timeframe=None, pair="BTC/USDT", limit=20):
-    #     if self.exchange.has['fetchOHLCV']:
-    #         historical_price = []
+    #  this should be used for static pooling the exchange (real-time data fetch)
+    def get_current_price_data(self, ticker="BTC/USDT", delay=10):
+        if(self.exchange.has['fetchTicker']):
+            print("############## THE FETCH STREAM BEGINS NOW ################### (to interupt C-c)")
+            while True:
+                data = self.exchange.fetch_ticker(ticker)['info']
+                # print(data)
+                print("symbol: {}, last price: {}, last qty: {}"\
+                        .format(data["symbol"], data["lastPrice"], data["lastQty"]))
+                print("askPrice: {}, askQty: {}, bidPrice: {}, bidQty: {}"\
+                    .format(data["askPrice"], data["askQty"], data["bidPrice"], data["bidQty"]))
+                time.sleep(delay)
 
-        
     def get_timeframes(self):
         try:
             print("Timeframes for {} are {}".format(self.exchange.name, self.exchange.timeframe))
@@ -74,8 +95,13 @@ def show_exchanges():
 
 if __name__ == '__main__':
     #  for full list of exchanges, please visit this link: https://github.com/ccxt/ccxt/wiki/Manual#instantiation
-    show_exchanges()
+    # show_exchanges()
     exchange = CryptoFetchtron()
-    exchange.get_timeframes()
-    exchange.show_pairs('BTC')
-    exchange.get_data_current()
+
+    #  this function takes absolute path where the file.db will be stored
+    exchange.connect_to_db("/home/peterson/Desktop/FinApp/NYU-ML/CryptoFetchtron/crypto_data/")
+
+    # exchange.get_timeframes()
+    # exchange.show_pairs('BTC')
+    # exchange.get_data_current()
+    exchange.get_current_price_data("BTC/USDT")
